66c8f8ac2289489e10776730169d7050e98f8cfb
TheTanishqChauhan <155085790+thetanishqchauhan@users.noreply.github.com>
2025-04-30 08:48:36 +0000

    modified:   implementStack.java

--- implementStack.java
+++ implementStack.java
@@ -1,13 +1,21 @@
 public class implementStack {
 
     public static void main(String[] args) {
-        stack s = new stack();
-        s.push(7);
-        s.push(8);
-        s.push(9);
-        System.out.println("Deleted value: " + s.pop());
-        System.out.println("Value at top: " + s.top());
-        s.pop();
+        // stack s = new stack();
+        // s.push(7);
+        // s.push(8);
+        // s.push(9);
+        // System.out.println("Deleted value: " + s.pop());
+        // System.out.println("Value at top: " + s.top());
+        // s.pop();
+
+        queue q = new queue();
+        q.push(6);
+        q.push(8);
+        q.push(10);
+        System.out.println("Deleted value: " + q.pop());
+        System.out.println("Value at top: " + q.top());
+        
     }
     
 }
@@ -34,6 +42,29 @@ class stack{
 
     int size(){
         return top + 1;
+    }
+    
+}
+
+class queue {
+    int size = 10;
+    int arr[] = new int[size];
+    int top = size - 1;
+    int current = size;
+
+    void push(int x){
+        current -= 1;
+        arr[current] = x;
+    }
+
+    int pop(){
+        int x = arr[top];
+        top -= 1;
+        return x;
+    }
+
+    int top(){
+        return arr[top];
     }
     
 }
